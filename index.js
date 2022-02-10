41bc9a7efaf3b7f5eaaa950a2d48943549a3bb60
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2022-02-10 20:19:59 +0530

    Overall

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-alert("WELCOME");
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
 //Toggle Dark Mode
 function ToggleTheme() {
   var element = document.body;
-  //var element = document.getElementById("dmode");
-  element.classList.toggle("dark-mode");
+  element.classList.toggle("dark");
+}
+
+function changebackground(){
+	document.body.style.backgroundColor = 'green';
 }
